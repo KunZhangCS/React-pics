a651a71d718e233d035c7a7c912a5c4918203cdd
Kun Zhang <kunzhang1988@gmail.com>
2020-03-09 13:02:37 +1000

    Use ref's and add grid css

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ React app
 1. Handling user input with Forms and Events
 2. Making API requests with React
 3. Building lists of records
+4. Using Ref's for DOM access
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,14 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
+
 
 const ImageList = props => {
-    // restructure out the 3 properties of image and don't need write id.xx three times
-    const images = props.images.map( ({description, id, urls}) => { 
-        return <img alt={description} key={id} src={urls.regular} />
+    const images = props.images.map( image => { 
+        return <ImageCard key={image.id} image={image} />
     });
 
-    return <div>{images}</div>
+    return <div className="image-list">{images}</div>
 };
 
 export default ImageList;
